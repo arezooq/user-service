1359d622a3b20be6b969ecf5ec2166b79f80b594
arezooq <ghorbanzadeharezoo@gmail.com>
2025-10-15 01:26:11 +0330

    Edit file constant

--- internal/constant/constant.go
+++ internal/constant/constant.go
@@ -2,34 +2,12 @@ package constant
 
 import (
 	"os"
-
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
-// LoginRequest
-type LoginRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
-
-// LoginResponse
-type LoginResponse struct {
-	ID           uuid.UUID `json:"id"`
-	Email        string    `json:"email"`
-	AccessToken  string    `json:"access_token"`
-	RefreshToken string    `json:"refresh_token"`
-}
-
-type OAuthResponse struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name"`
-}
-
 // service info
 const (
-	ServiceName    = "auth-service"
+	ServiceName    = "user-service"
 	ServiceVersion = "1.0.0"
 )
 
